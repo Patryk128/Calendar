728499d9474fef24c8a009a7a1899f25cad2a807
Patryk Krawczyk <patrykkrawczykk157@gmail.com>
2024-06-27 20:08:24 +0200

    callendar

--- src/index.js
+++ src/index.js
@@ -5,17 +5,8 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebase.js";
 import "./ReactBigCalendar.css";
 import "./App.css";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCKYKhwSDwyL356mju8g9OHlFy3k5qIyAc",
-//   authDomain: "calendar-5ca33.firebaseapp.com",
-//   databaseURL: "https://calendar-5ca33-default-rtdb.firebaseio.com",
-//   projectId: "calendar-5ca33",
-//   storageBucket: "calendar-5ca33.appspot.com",
-//   messagingSenderId: "572647972220",
-//   appId: "1:572647972220:web:30ca953ac5553e50ddd8f9",
-//   measurementId: "G-MH6S5ZWF7X",
-// };
+import "./Buttons.css";
+import "./Popup.css";
 
 initializeApp(firebaseConfig);
 
